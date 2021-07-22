bd2451cb8ffe0f28d5d19acb600aa367245868dc
AdBlox <43703390+LightningBlox@users.noreply.github.com>
2021-07-22 23:58:39 +0500

    Update loader.js

--- source/loader.js
+++ source/loader.js
@@ -4,7 +4,6 @@ function move() {
     i = 1;
     var elem = document.getElementById("bar-main");
     var width = 1;
-    var height = -1;
     var id = setInterval(frame, 10);
     function frame() {
       if (width >= 100) {
@@ -13,7 +12,6 @@ function move() {
       } else {
         width++;
         elem.style.width = width + "%";
-        elem.style.width = height + "%";
         elem.remove();
       }
     }
