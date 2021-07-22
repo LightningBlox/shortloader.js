95b127ae46c088b433824914958f1137b65a63cd
AdBlox <43703390+LightningBlox@users.noreply.github.com>
2021-07-23 00:09:16 +0500

    Update loader.js

--- source/loader.js
+++ source/loader.js
@@ -12,9 +12,19 @@ function move() {
       } else {
         width++;
         elem.style.width = width + "%";
+        elem.remove();
       }
     }
   }
+}
+
+  console.log("%cShortLoader.js! Making the web fast!", "color: #a0b5c3; font-family: sans-serif; font-size: 2em; font-weight: bolder;");
+  console.log("%cVanillaJS, NO Dependencies!", "color: teal; font-family: sans-serif; font-size: 1em; font-weight: bolder;");
+
+function removeElems() {
+  
+  console.log("%cShortLoader.js has been loaded! Thank you for using *SHORTLOADER*", "color: #a0b5c3; font-family: sans-serif; font-size: 2em; font-weight: bolder;");
+ 
 }
 
 // Update Loader.JS
